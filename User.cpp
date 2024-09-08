77547319ac0af6441b19109b58ef95abb5f5fcf8
Roman Kolosok <romakolosok14@gmail.com>
2024-09-08 12:27:31 -0400

    recent changes 9/8

--- User.cpp
+++ User.cpp
@@ -5,13 +5,17 @@ using namespace std;
 
 int main() {
 
-	string a, b, c, d;
+	string a, b, c;
 	cin >> a;
 	cin >> b;
 	cin >> c;
 
 	cout << a << endl;
 	cout << b << endl;
+	cout << c << endl;
+	cout << c << endl;
+	cout << c << endl;
+	cout << c << endl;
 	cout << c << endl;
 
 	return 0;
